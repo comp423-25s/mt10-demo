7e2875ae3b15ebd7b6c0d0ccffc0beaf5a4aea14
Kris Jordan <krisjordan@gmail.com>
2025-02-05 15:41:39 +0000

    Add examples for Meeting 10

--- main.py
+++ main.py
@@ -1,7 +1,50 @@
-"""FastAPI main entrypoint file."""
+"""An API for Thingies!"""
 
-from fastapi import FastAPI
+__author__ = "Kris Jordan <kris@cs.unc.edu>"
+
+from typing import Annotated
+from fastapi import FastAPI, Body, Response, Path
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-# TODO: Add routes here.
+
+class Thingy(BaseModel):
+    """A Thingy!"""
+
+    id: Annotated[str, Field(description="Resource ID", examples=["1234"])] = ""
+
+    redirect_url: Annotated[
+        str, Field(description="URL redirected to", examples=["https://csxl.unc.edu"])
+    ] = ""
+
+    snippet_text: Annotated[
+        str, Field(description="Snippet text", examples=["Hello, world"])
+    ] = ""
+
+
+"""A 'Database' of Thingies"""
+thingies_db: dict[str, Thingy] = {}
+
+
+@app.post("/", summary="Create a Thingy")
+def thingy_new(
+    thingy: Annotated[Thingy, Body(description="Thingy to Create")]
+) -> Thingy:
+    thingies_db[thingy.id] = thingy
+    return thingies_db[thingy.id]
+
+
+@app.get("/{thingy_id}", summary="Load a Thingy")
+def thingy_too(
+    thingy_id: Annotated[str, Path(description="Thingy Identifier")]
+) -> Response:
+    thingy: Thingy | None = thingies_db.get(thingy_id)
+    if thingy:
+        if thingy.snippet_text:
+            return Response(content=thingy.snippet_text, media_type="text/plain")
+        else:
+            return Response(status_code=302, headers={"Location": thingy.redirect_url})
+    else:
+        return JSONResponse(status_code=404, content={"error": "Resource not found"})
